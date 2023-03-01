072f552d09f7bdbf0b2e598065ad262c34a57db4
Tyler Hill <tylergenehill122@gmail.com>
2023-03-01 14:51:03 -0500

    summary stats table as HTML

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, redirect
+import data
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- data.py
+++ data.py
@@ -4,6 +4,14 @@ import pandas as pd
 import scipy.stats as st
 import random
 
+# Import JSON data
 mouse_data_path = "static/data/mouse_data.json"
 
+# Read into Pandas as a DataFrame
 df = pd.read_json(mouse_data_path)
+
+# Summary Stats function
+def summary_stats():
+    drug_df = df.groupby("Drug Regimen")
+    drug_summary_df = drug_df.agg(["mean","median","var","std","sem"])["Tumor Volume (mm3)"]
+    return drug_summary_df.to_html()
